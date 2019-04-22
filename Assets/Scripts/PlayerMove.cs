7dbf85b6b45e1d59a7a677d35c38ef4f994b5fb4
Gideon Clegg <19.gideon.clegg@ksd.org>
2019-04-22 10:45:04 -0700

    Basic robot controls working

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isControlled = false;
+    private Rigidbody rb;
+    public float moveSpeed = 1;
+    public float turnSpeed = 1;
+    public bool grounded = false;
+    public bool upright = true;
+    public bool rightObject = false;
+    public int rightObjectStage = 0;
+    public float uprightTimeout = 3f;
+    public float uprightLiftTime = 1f;
+    public float uprightDuration = 1f;
+    public float uprightPause = 1f;
+    private float upsideDownTime;
+    private float uprightTimer = 0;
+    private Vector3 curPos;
+    private Quaternion curRot;
+
+    private void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (rightObject)
+        {
+            uprightTimer += Time.deltaTime;
+            if (rightObjectStage == 0)
+            {
+                transform.position = Vector3.Lerp(curPos, curPos + Vector3.up, uprightTimer / uprightLiftTime);
+                if (uprightTimer > uprightLiftTime)
+                {
+                    transform.position = curPos + Vector3.up;
+                    uprightTimer = 0;
+                    rightObjectStage++;
+                }
+            }
+            if(rightObjectStage == 1)
+            {
+                transform.rotation = Quaternion.Lerp(curRot, Quaternion.Euler(0, curRot.eulerAngles.y, 0), uprightTimer / uprightDuration);
+                if(uprightTimer > uprightDuration)
+                {
+                    transform.rotation = Quaternion.Euler(0, curRot.eulerAngles.y, 0);
+                    uprightTimer = 0;
+                    rightObjectStage++;
+                }
+            }
+            if(rightObjectStage == 2)
+            {
+                if(uprightTimer > uprightPause)
+                {
+                    uprightTimer = 0;
+                    rightObjectStage = 0;
+                    rightObject = false;
+                    rb.isKinematic = false;
+                }
+            }
+        }
+        else
+        {
+            upsideDownTime += Time.deltaTime;
+            Debug.Log(rb.velocity);
+            if (rb.velocity.y > -0.1 && rb.velocity.y < 0.1) { grounded = true; } else { grounded = false; upsideDownTime = 0; }
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down), Color.cyan, 0.5f);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 0.5f)) { upright = true; upsideDownTime = 0; } else { upright = false; }
+            if (isControlled && grounded && upright)
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime, 0.5f);
+                if (Input.GetAxisRaw("Horizontal") != 0)
+                {
+                    transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime);
+                }
+            }
+            if (upsideDownTime > uprightTimeout)
+            {
+                upsideDownTime = 0;
+                rightObject = true;
+                uprightTimer = 0f;
+                rb.isKinematic = true;
+                curPos = transform.position;
+                curRot = transform.rotation;
+            }
+        }
     }
 }
